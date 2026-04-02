c6001312bb9b83a87723eaf1ee660bfc8afacdb4
leekelleher <leekelleher@gmail.com>
2026-04-03 00:24:12 +0100

    Convert write-good from UMD bundle to ESM module
    Refactored the Browserify bundle into a clean ES module with proper
    exports. All checker functions and word lists preserved. Tool now
    imports writeGood as a default ESM import.

--- tools/write-good/index.js
+++ tools/write-good/index.js
@@ -1,3 +1,4 @@
+import writeGood from './write-good.esm.js';
 import { openDismissablePanel, dismissDismissablePanels } from '../../app/ui.js';
 
 function updateWriteGood(scratchpad) {
@@ -8,7 +9,6 @@ function updateWriteGood(scratchpad) {
     let results = writeGood(html);
 
     for (let r of results.reverse()) {
-        console.log(r);
         html = html.substring(0, r.index) +
             "<span class='highlight' aria-label='" + r.reason + "' data-balloon-pos='down-left'>" +
             html.substring(r.index, r.index + r.offset) +
