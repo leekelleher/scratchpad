08791950efa93ba8e6b757532715e311c13725eb
leekelleher <leekelleher@gmail.com>
2026-04-03 00:34:48 +0100

    Replace jwt-decode with ESM build (v4)

--- tools/jwt/index.js
+++ tools/jwt/index.js
@@ -1,3 +1,4 @@
+import { jwtDecode } from './jwt-decode.esm.js';
 import { displayError } from '../../app/ui.js';
 
 export default {
@@ -5,8 +6,8 @@ export default {
     footer: true,
     action(scratchpad) {
         try {
-            var token = jwt_decode(scratchpad.value);
-            var decodedHeader = jwt_decode(scratchpad.value, { header: true });
+            var token = jwtDecode(scratchpad.value);
+            var decodedHeader = jwtDecode(scratchpad.value, { header: true });
             scratchpad.value = JSON.stringify(decodedHeader, null, 2) + "\n" + JSON.stringify(token, null, 2);
         } catch (e) {
             displayError(e.message);
