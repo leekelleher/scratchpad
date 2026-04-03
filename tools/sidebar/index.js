49da302ca644ab9b2a90ca2c6c60e5e5aed8f1ef
leekelleher <leekelleher@gmail.com>
2026-04-03 10:42:45 +0100

    Use flexbox column layout for sidebar nav

--- tools/sidebar/index.js
+++ tools/sidebar/index.js
@@ -2,9 +2,9 @@ export default {
     name: 'sidebar',
     footer: true,
     action() {
-        let sidebarEl = document.querySelector("#sidebar");
-        let isOpen = sidebarEl.style.display == 'block';
-        sidebarEl.style.display = isOpen ? 'none' : 'block';
+        const sidebarEl = document.getElementById("sidebar");
+        const isOpen = sidebarEl.style.display == 'flex';
+        sidebarEl.style.display = isOpen ? 'none' : 'flex';
         sidebarEl.setAttribute('aria-hidden', isOpen ? 'true' : 'false');
     }
 };
